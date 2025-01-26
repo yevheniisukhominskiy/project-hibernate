d7fe9d8a46e10766f3c65454dc6f096c378874af
Eugene Sukhominskiy <yevhenii.sukhominskiy@gmail.com>
2025-01-26 22:00:01 +0200

    Initial commit

---
+++ src/main/java/by/eugene/dao/CityDAO.java
@@ -0,0 +1,36 @@
+package by.eugene.dao;
+
+import by.eugene.entity.City;
+import by.eugene.entity.CountryLanguage;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+import java.util.Set;
+
+public class CityDAO {
+    private final SessionFactory sessionFactory;
+
+    public CityDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public List<City> getItems(int offset, int limit) {
+        Query<City> query = sessionFactory.getCurrentSession().createQuery("select c from City c", City.class);
+        query.setFirstResult(offset);
+        query.setMaxResults(limit);
+        return query.list();
+    }
+
+    public int getTotalCount() {
+        Query<Long> query = sessionFactory.getCurrentSession().createQuery("select count(c) from City c", Long.class);
+        return Math.toIntExact(query.uniqueResult());
+    }
+
+    public City getById(Integer id) {
+        Query<City> query = sessionFactory.getCurrentSession().createQuery("select c from City c join fetch c.country where c.id = :ID", City.class);
+        query.setParameter("ID", id);
+        return query.getSingleResult();
+    }
+}
---
+++ src/main/java/by/eugene/entity/Continent.java
@@ -0,0 +1,11 @@
+package by.eugene.entity;
+
+public enum Continent {
+    ASIA,
+    EUROPE,
+    NORTH_AMERICA,
+    AFRICA,
+    OCEANIA,
+    ANTARCTICA,
+    SOUTH_AMERICA
+}
